7f3bece909fe3cb4c6e5009fb7ea172eca379a85
francobelloni85 <francobelloni85@users.noreply.github.com>
2022-10-11 17:43:19 +0200

    [add] exercises 4

--- 4_if_numeri.py
+++ 4_if_numeri.py
@@ -4,3 +4,20 @@
 # Se il secondo numero è maggiore di 50 scrivi "TROPPO GRANDE". 
 # Se non hai stampato nulla allora stampa "TUTTO OK"
 # ---------------------
+
+n1 = int(input("Inserisci il primo numero"))
+n2 = int(input("Inserisci il secondo numero"))
+
+# varibile che mi segna se ho già stampato qualcosa...
+stampato = 0
+
+if(n1 > 10):
+    print("Fuori Scala")
+    stampato = 1
+     
+if(n2 > 50):
+    print("Troppo Grande")
+    stampato = 1
+
+if(stampato == 0):
+    print("Tutto ok")
