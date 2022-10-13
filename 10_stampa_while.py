0e341af86a3ec2f30d7581657ed7534b70634854
francobelloni85 <francobelloni85@users.noreply.github.com>
2022-10-13 10:50:17 +0200

    [ADD] esercizio intro while

---
+++ 10_stampa_while.py
@@ -0,0 +1,12 @@
+# ----- CONSEGNA ------ 
+# Stampa i primi 10 numeri con il cliclo while.
+# ---------------------
+
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+
+
+# exit
+lettera = input("press any key to exit")
