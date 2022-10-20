69029321da1f20c24baa9ea97aca4caa4ad44ce6
francobelloni85 <francobelloni85@users.noreply.github.com>
2022-10-20 15:03:55 +0200

    [ADD] Correzione esercizi da 11 a 14

--- 11_numero_magico.py
+++ 11_numero_magico.py
@@ -5,15 +5,18 @@
 # Se indovina stampa "Complimenti, hai indovinato!" 
 # Se non indovina stampa "Game over!"
 # ---------------------
+
 numero_magico = 7
 numero = 0
 output = "Game Over!"
-i = 1
-while i <= 10:
-    numero = int(input("Prova ad indovinare il numero magico: "))
+i = 0
+numero = int(input("Prova ad indovinare il numero magico: "))
+while i < 10:    
     if(numero == numero_magico):
         output = "Complimenti, hai indovinato!"        
         break
+    else:
+        numero = int(input("hai sbagliato, ritenta: "))        
     i = i + 1
 print(output)
  
---
+++ 15_media_while.py
@@ -0,0 +1,18 @@
+# ----- CONSEGNA ------ 
+# Calcola la media di tutti i numeri che l'utente ha inserito fino a quel momento.
+# Quando l'utente inserisce il numero 0 allora stampa il risultato.
+# ---------------------
+
+totale = 0
+count_user_number = 0
+numero = 0 
+i = 1
+
+while True :
+    numero = int(input('inserisci numeri'))
+
+
+
+
+
+a=input("Press any key to exit")
